4bbe07092ba65fbf39970686fe7bb8cd0e322424
raharshi puli <raharshi.puli@gmail.com>
2023-10-02 17:05:21 +0530

    first commit

---
+++ src/main/java/com/untangle/dsalgoprep/DsalgoprepApplication.java
@@ -0,0 +1,13 @@
+package com.untangle.dsalgoprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsalgoprepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsalgoprepApplication.class, args);
+	}
+
+}
